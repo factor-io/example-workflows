c4bd9418765cc9c46618a0b616e45458b64d44b1
Maciej Skierkowski <maciej@skierkowski.com>
2014-08-13 17:02:02 -0700

    Updating per new interface

--- timed-web-post.rb
+++ timed-web-post.rb
@@ -1,4 +1,4 @@
-listen 'timer','every', minutes:1 do |post_info|
-  run 'web','post', address:'http://requestb.in/qbx6puqb'
-  run 'web','post', address:'http://requestb.in/1iu7m1d1'
+listen 'timer::every', minutes:1 do |post_info|
+  run 'web::post', url:'http://requestb.in/qbx6puqb'
+  run 'web::post', url:'http://requestb.in/1iu7m1d1'
 end

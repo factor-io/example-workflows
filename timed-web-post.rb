7659e40c239dc2d69ec8aa16dd30a4baf903bd49
Taylor Brown <taytay@taytay.com>
2014-08-31 15:13:50 -0500

    Whoops - missed a comma!

--- timed-web-post.rb
+++ timed-web-post.rb
@@ -1,7 +1,7 @@
 listen 'timer::every', minutes:1 do |post_info|
   vars = {
     message: "Hello! This is a message from Factor.io",
-    heroku_app_name: ENV['URL'].split(".").first
+    heroku_app_name: ENV['URL'].split(".").first,
     current_server_time: Time.now.inspect
   }
   puts "Timer fired. About to post a message."

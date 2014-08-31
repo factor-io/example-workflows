cee2c2172327a415704afdae8f4f51631deeb74c
Taylor Brown <taytay@taytay.com>
2014-08-31 15:16:55 -0500

    Removing fanciness to find out why it's not working

--- timed-web-post.rb
+++ timed-web-post.rb
@@ -1,9 +1,9 @@
 listen 'timer::every', minutes:1 do |post_info|
-  vars = {
-    message: "Hello! This is a message from Factor.io",
-    heroku_app_name: ENV['URL'].split(".").first,
-    current_server_time: Time.now.inspect
-  }
-  puts "Timer fired. About to post a message."
+  # vars = {
+  #   message: "Hello! This is a message from Factor.io",
+  #   heroku_app_name: ENV['URL'].split(".").first,
+  #   current_server_time: Time.now.inspect
+  # }
+  # puts "Timer fired. About to post a message."
   run 'web::post', url:'http://requestb.in/yokqwiyo', params:vars
 end

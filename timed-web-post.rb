7aa0afc4190da85b1e0d95f77894842278256af8
Taylor Brown <taytay@taytay.com>
2014-08-31 14:56:41 -0500

    Add debug line to timed-web-post.rb

--- timed-web-post.rb
+++ timed-web-post.rb
@@ -4,5 +4,6 @@ listen 'timer::every', minutes:1 do |post_info|
     heroku_app_name: ENV['URL'].split(".").first
     current_server_time: Time.now.inspect
   }
+  puts "Timer fired. About to post a message."
   run 'web::post', url:'http://requestb.in/yokqwiyo', params:vars
 end

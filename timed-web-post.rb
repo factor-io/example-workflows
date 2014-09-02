7b34fd74ad711b621f413bc41247117a03eeff6a
Taylor Brown <taytay@taytay.com>
2014-09-02 10:10:32 -0500

    Remove commented code

--- timed-web-post.rb
+++ timed-web-post.rb
@@ -1,9 +1,4 @@
 listen 'timer::every', minutes:1 do |post_info|
-  # vars = {
-  #   message: "Hello! This is a message from Factor.io",
-  #   heroku_app_name: ENV['URL'].split(".").first,
-  #   current_server_time: Time.now.inspect
-  # }
-  # puts "Timer fired. About to post a message."
-  run 'web::post', url:'http://requestb.in/yokqwiyo', params:vars
+  # Get your own requestb.in URL here: http://requestb.in/
+  run 'web::post', url:'http://requestb.in/yokqwiyo'
 end
